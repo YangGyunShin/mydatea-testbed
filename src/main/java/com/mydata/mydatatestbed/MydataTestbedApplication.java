d459864925493e80b71adeb2a77b29f9fb39117e
YangGyunShin <skshtlsdidrb@naver.com>
2026-01-17 16:13:49 +0900

    Initial commit: Spring Boot project setup

---
+++ src/main/java/com/mydata/mydatatestbed/MydataTestbedApplication.java
@@ -0,0 +1,13 @@
+package com.mydata.mydatatestbed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MydataTestbedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MydataTestbedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mydata/mydatatestbed/MydataTestbedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mydata.mydatatestbed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MydataTestbedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
